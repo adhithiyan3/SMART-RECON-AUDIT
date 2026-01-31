f44c94aadfddce8cd52d1e95bdef39ed86552cd0
adhithiyan3 <adhithiyan921@gmail.com>
2026-01-31 16:56:45 +0530

    set url

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Enable CORS for frontend
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: ['http://localhost:5173', 'https://smart-recon-audit.vercel.app'],
   credentials: true
 }));
 
